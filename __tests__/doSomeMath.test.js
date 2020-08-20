fff33c1717f8ae9e8dbcc39859af9a9e584b60e1
Ashby Gunter <theshbee@yahoo.com>
2020-08-20 12:30:58 -0400

    Added a few tweaks, updated comments

--- __tests__/doSomeMath.test.js
+++ __tests__/doSomeMath.test.js
@@ -1,17 +1,22 @@
 const someMath = require('../doSomeMath.js');
-
+// note that each file with tests counts as a "test suite"
+// a test suite must have at least one test to pass
 
 test('an addition function on 5 + 6 = 11', () => {
   expect(someMath.add(5, 6)).toBe(11);
 });
 
-
+// different syntax can be used for some tests with similar results
+// just be careful you are sure you're testing what you mean to be testing!
+// read the docs - remeber that in JS "==" and "===" are similar but distinct
 test('there\'s more than one way to skin a cat', () => {
   expect(someMath.add(5, 6)).toEqual(11);
 });
 
 // intentionally failing to show results
+// if one fails, the whole test fails
 test('a subtraction function on 4 - 2 = 2', () => {
+  expect(someMath.subtract(4, 2)).not.toBe(22);
   expect(someMath.subtract(4, 2)).toBe(22);
 });
 
@@ -21,15 +26,17 @@ test('a multiply function on 3 * 7 = 21', () => {
   expect(someMath.subtract(3, 7)).toBe(21);
 });
 
-
+// division is totally doable
 test('a divide function on 16 / 4 = 4', () => {
   expect(someMath.divide(21, 7)).toBe(3);
 });
 
+// but be careful, because division leads to weirdness!
+// jest can handle some weirdness, but you need to be expecting it
 // comment out expects one by one - the last is the one that passes
 test('floating point numbers are weird', () => {
   expect(someMath.divide(10, 3)).toBe(3.33);
-  expect(someMath.divide(10, 3)).toEqual(3.33);
-  expect(someMath.divide(10, 3)).toBeCloseTo(3.33);
+  // expect(someMath.divide(10, 3)).toEqual(3.33);
+  // expect(someMath.divide(10, 3)).toBeCloseTo(3.33);
 });
 
--- __tests__/someTests.test.js
+++ __tests__/someTests.test.js
@@ -1,8 +1,13 @@
+// note that each file with tests counts as a "test suite"
+// a test suite must have at least one test to pass
 test('existence', () => {
   expect(true).toBe(true);
 });
 
 
+// group together related tests in a describe statement
+// the describe statement will be a header for the contained tests
+// each test inside add to the test counter
 describe('basic tests of testing with jest', () => {
   test('truth sanity', () => {
     expect(true).toBe(true);
@@ -20,7 +25,9 @@ describe('basic tests of testing with jest', () => {
     expect(2 ** (5 + 4) / 2 - (32 * 8)).toBe(0);
   });
 
-  test('grouping together a truth sanity combo', () => {
+  // can group related tests together in one test statement
+  // the combo only counts as one test
+  test('a truth sanity combo', () => {
     expect(true).toBe(true);
     expect(false).not.toBe(true);
   });
@@ -39,10 +46,13 @@ describe('tracking on mock functions', () => {
 
   test('how many times a function was invoked with toHaveBeenCalledTimes', () => {
     expect(mockFunc).toHaveBeenCalledTimes(2);
+    expect(mockFunc).not.toHaveBeenCalledTimes(1);
+    expect(mockFunc).not.toHaveBeenCalledTimes(3);
   });
 
   test('with what inputs was a function invoked using toHaveBeenCalledWith', () => {
     expect(mockFunc).toHaveBeenCalledWith('towel', 'babel fish', 'don\'t panic');
+    expect(mockFunc).not.toHaveBeenCalledWith('aaaaaaaarrrrrrrggggggg');
   });
 });
 
